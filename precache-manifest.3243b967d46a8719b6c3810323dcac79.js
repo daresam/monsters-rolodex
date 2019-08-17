f911911ba97a2643d711cee6c83586e62fa9ad1c
Dare Sam <dawakesam@gmail.com>
2019-08-17 14:09:42 +0100

    Updates

---
+++ precache-manifest.3243b967d46a8719b6c3810323dcac79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975b707794c8886738d885fb1a5d93d2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1b23bc36b21a679b97d4",
+    "url": "/monsters-rolodex/static/css/main.da1c0ddf.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1b23bc36b21a679b97d4",
+    "url": "/monsters-rolodex/static/js/main.8115b373.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
